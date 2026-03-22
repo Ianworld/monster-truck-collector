64ae0a96f6944530a46768764071c81cbe9301cc
Ian Ferguson <ianferguson@example.com>
2026-03-21 22:29:20 -0400

    Add GitHub Pages deployment config

---
+++ client/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // This tells Vite that the site will be hosted at /monster-truck-collector/
+  base: '/monster-truck-collector/',
+})
